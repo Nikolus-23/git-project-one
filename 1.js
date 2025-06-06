5f57afea93ee6d379fcd0babac33d93038b60572
kazem <opads>
2025-06-06 10:45:09 +0300

    starts-test

---
+++ 1.js
@@ -0,0 +1,13 @@
+alert('hello world');
+
+function fun() {
+
+    if (names === 'ali') {
+        alert('ali heLLo and welcome to site')
+    }
+}
+
+
+var names = prompt('number is ?')
+
+alert(fun())
